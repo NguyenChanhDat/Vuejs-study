23dd4497350079b3a679875d7240674304c2dda0
NguyenChanhDat <chanhdat2504@gmail.com>
2025-12-28 02:02:48 +0700

    feat: add Login form

---
+++ src/components/LoginForm.vue
@@ -0,0 +1,34 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+
+type LoginFormData = {
+  username: string
+  password: string
+}
+
+const userInformation = ref<LoginFormData>({
+  username: '',
+  password: '',
+})
+
+const handleSubmit = (e: Event) => {
+  e.preventDefault()
+  console.log('Username:', userInformation.value.username)
+  console.log('Password:', userInformation.value.password)
+}
+</script>
+
+<template>
+  <h1>Login Form</h1>
+  <form>
+    <div>
+      <label for="username">Username:</label>
+      <input v-model="userInformation.username" type="text" id="username" name="username" />
+    </div>
+    <div>
+      <label for="password">Password:</label>
+      <input v-model="userInformation.password" type="password" id="password" name="password" />
+    </div>
+    <button @click="handleSubmit" type="submit">Login</button>
+  </form>
+</template>
